e0e9a6c9828d03e65950c4888f8d358af7fcc5a8
laizu akram <laizuakram98@gmail.com>
2023-02-18 13:10:36 +0600

    create banner slider

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Banner.css';
+import slide from '../../../assets/images/slider/slide.jpg';
+import slide2 from '../../../assets/images/slider/slide2.jpg';
+import slide3 from '../../../assets/images/slider/slide3.jpg';
+
+const Banner = () => {
+
+    return (
+        <div>
+            <div className="carousel w-full h-96">
+                <div id="slide1" className="carousel-item relative w-full">
+                    {/* slide content */}
+                    <div className="hero h-96" style={{ background: `url('${slide}') no-repeat center center` }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    {/* slide content */}
+                <div className="hero h-96" style={{ background: `url('${slide2}') no-repeat center center` }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    
+                    {/* slide content */}
+                    <div className="hero h-96" style={{ background: `url('${slide3}') no-repeat center center` }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
+import Banner from './Banner/Banner';
+
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
+            
         </div>
     );
 };
