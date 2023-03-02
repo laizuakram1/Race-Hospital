9b89431de6ba16dccbdb8d644b2e4eb7ad4bf9f1
laizu akram <laizuakram98@gmail.com>
2023-03-02 14:42:47 +0600

    send report file in database and display user specific report

--- src/Pages/Dashboard/Report/Report.js
+++ src/Pages/Dashboard/Report/Report.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import { AuthContext } from '../../../Contexts/AuthProvider';
 
 const Report = () => {
     const { register, handleSubmit } = useForm();
+    const { auth } = useContext(AuthContext)
+    const email = auth?.currentUser?.email
+    const [reports, setReports] = useState([])
+    console.log(reports)
     const imageHostKey = process.env.REACT_APP_imagebb_host_key;
+    const [refetch, setRefetch] = useState({})
 
 
     const onSubmit = data => {
         const test = data.test;
-        const image = data.report[0]
+        const image = data.Report[0]
 
         const formData = new FormData();
         formData.append("image", image);
@@ -20,12 +26,13 @@ const Report = () => {
         })
             .then((res) => res.json())
             .then((data) => {
-                
+
                 const patientInfo = {
-                    test: test,
+                    test,
+                    email,
                     image: data.data.url
                 }
-                fetch(`http://localhost:5000/report`, {
+                fetch(`http://localhost:5000/reports`, {
                     method: 'POST',
                     headers: {
                         'Content-type': 'application/json'
@@ -35,6 +42,7 @@ const Report = () => {
                     .then(res => res.json())
                     .then(data => {
                         toast.success('Report upload successfull.')
+                        setRefetch(data)
                     })
 
 
@@ -42,21 +50,39 @@ const Report = () => {
             .catch((error) => {
                 console.error("Error:", error);
             });
-
-
-
-
     }
+
+    useEffect(() => {
+        const url = `http://localhost:5000/reports?email=${email}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setReports(data))
+    }, [refetch])
+
 
     return (
         <div>
             <div>
                 <h3 className='text-3xl text-cyan-400 m-2'>Test Report</h3>
                 <form onSubmit={handleSubmit(onSubmit)} className='p-3 grid md:grid-cols-3 gap-5'>
-                    <input {...register("test",{requred: "please provide test name"})} type="text" placeholder="Test name" className="input input-bordered w-full max-w-xs" />
-                    <input {...register('Report', {required: true})} type="file" className="file-input file-input-bordered file-input-accent w-full max-w-xs" />
+                    <input {...register("test", { requred: "please provide test name" })} type="text" placeholder="Test name" className="input input-bordered w-full max-w-xs" />
+                    <input {...register('Report', { required: true })} type="file" className="file-input file-input-bordered file-input-accent w-full max-w-xs" />
                     <input className='text-start btn btn-outline btn-accent w-2/4' type="submit" />
                 </form>
+
+                <div className='grid md:grid-cols-3 gap-5'>
+                    {
+                        reports.map((report, i) => <div className="card w-58 bg-base-100 shadow-xl"
+                            key={i}
+                        >
+                            <figure className="px-10 pt-10">
+                                <img src={report.image} alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <p className='text-sm text-center uppercase py-3'>{report.test}</p>
+
+                        </div>)
+                    }
+                </div>
             </div>
 
         </div>
