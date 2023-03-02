a0bd9fecb41673f85847335ae746b802e7aaff9e
laizu akram <laizuakram98@gmail.com>
2023-03-02 12:01:18 +0600

    display user specififc prescription

--- src/Pages/Dashboard/Prescription/Prescription.js
+++ src/Pages/Dashboard/Prescription/Prescription.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import { AuthContext } from '../../../Contexts/AuthProvider';
 
 const Prescription = () => {
     const { register, handleSubmit } = useForm();
+    const { auth } = useContext(AuthContext);
     const imageHostKey = process.env.REACT_APP_imagebb_host_key;
+    const [prescriptions, setPrescription] = useState([])
+    const email = auth?.currentUser?.email
+    const [refetch, setRefetch] = useState({})
 
 
     const onSubmit = data => {
-        const name = data.name;
+        const name = data.name
         const image = data.prescription[0]
 
         const formData = new FormData();
@@ -22,7 +27,8 @@ const Prescription = () => {
             .then((data) => {
 
                 const patientInfo = {
-                    name: name,
+                    name,
+                    email,
                     image: data.data.url
                 }
                 fetch(`http://localhost:5000/prescription`, {
@@ -35,6 +41,7 @@ const Prescription = () => {
                     .then(res => res.json())
                     .then(data => {
                         toast.success('upload successfull.')
+                        setRefetch(data)
                     })
 
 
@@ -43,21 +50,38 @@ const Prescription = () => {
                 console.error("Error:", error);
             });
 
-
-
-
     }
 
+    useEffect(() => {
+        const url = `http://localhost:5000/prescription?email=${email}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setPrescription(data))
+    }, [refetch])
 
 
     return (
         <div>
             <h3 className='text-3xl text-cyan-400 m-2'>All Prescriptions</h3>
             <form onSubmit={handleSubmit(onSubmit)} className='p-3 grid md:grid-cols-3 gap-5'>
-                <input {...register("name")} type="text" placeholder="patient name" className="input input-bordered w-full max-w-xs" />
-                <input {...register('prescription')} type="file" className="file-input file-input-bordered file-input-accent w-full max-w-xs" />
+                <input {...register("name", { required: 'please provide patient name' })} type="text" placeholder="patient name" className="input input-bordered w-full max-w-xs" />
+                <input {...register('prescription', { required: true })} type="file" className="file-input file-input-bordered file-input-accent w-full max-w-xs" />
                 <input className='text-start btn btn-outline btn-accent w-2/4' type="submit" />
             </form>
+
+            <div className='grid md:grid-cols-3 gap-5'>
+                {
+                    prescriptions.map((prescription, i) => <div className="card w-58 bg-base-100 shadow-xl"
+                        key={i}
+                    >
+                        <figure className="px-10 pt-10">
+                            <img src={prescription.image} alt="Shoes" className="rounded-xl" />
+                        </figure>
+                        <p className='text-sm text-center uppercase py-3'>{prescription.name}</p>
+
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/Report/Report.js
+++ src/Pages/Dashboard/Report/Report.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 
 const Report = () => {
+    const { register, handleSubmit } = useForm();
+    const imageHostKey = process.env.REACT_APP_imagebb_host_key;
+
+
+    const onSubmit = data => {
+        const test = data.test;
+        const image = data.report[0]
+
+        const formData = new FormData();
+        formData.append("image", image);
+
+        fetch(`https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`, {
+            method: "POST",
+            body: formData,
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                
+                const patientInfo = {
+                    test: test,
+                    image: data.data.url
+                }
+                fetch(`http://localhost:5000/report`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(patientInfo)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        toast.success('Report upload successfull.')
+                    })
+
+
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+
+
+
+
+    }
+
     return (
         <div>
-            <h3 className='text-3xl text-cyan-400 m-2'>Test Report</h3>
+            <div>
+                <h3 className='text-3xl text-cyan-400 m-2'>Test Report</h3>
+                <form onSubmit={handleSubmit(onSubmit)} className='p-3 grid md:grid-cols-3 gap-5'>
+                    <input {...register("test",{requred: "please provide test name"})} type="text" placeholder="Test name" className="input input-bordered w-full max-w-xs" />
+                    <input {...register('Report', {required: true})} type="file" className="file-input file-input-bordered file-input-accent w-full max-w-xs" />
+                    <input className='text-start btn btn-outline btn-accent w-2/4' type="submit" />
+                </form>
+            </div>
+
         </div>
     );
 };
